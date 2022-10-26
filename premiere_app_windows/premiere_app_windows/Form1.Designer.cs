f0de7c78d40ec060b534e9bb636510a5a48263a2
simonlac <101816652+simonlac@users.noreply.github.com>
2022-10-26 10:29:46 -0400

    Add files via upload

---
+++ premiere_app_windows/premiere_app_windows/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace premiere_app_windows
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Form_prin = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Form_prin
+            // 
+            this.Form_prin.Location = new System.Drawing.Point(102, 328);
+            this.Form_prin.Name = "Form_prin";
+            this.Form_prin.Size = new System.Drawing.Size(204, 47);
+            this.Form_prin.TabIndex = 0;
+            this.Form_prin.Text = "button1";
+            this.Form_prin.UseVisualStyleBackColor = true;
+            this.Form_prin.Click += new System.EventHandler(this.Form_prin_click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(528, 328);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(191, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button2";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Form_prin_click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label1.Location = new System.Drawing.Point(298, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Form_prin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Form_prin;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ premiere_app_windows/premiere_app_windows/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace premiere_app_windows
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        // private void Form_prin_click(object sender, EventArgs e)
+        //{
+        //Button senderButton = (Button)sender;
+        //if (senderButton.Text == "Valider") { senderButton.Text = "bonsoir"; }
+        //else senderButton.Text = "Valider";
+
+        //}
+        private void Form_prin_click(object sender, EventArgs e)
+        {
+            Button senderButton = (Button)sender;
+
+            if (senderButton.Name == "Form_prin") button1.Text = "button2";
+            else if (senderButton.Name == "button1") Form_prin.Text = "bouton 1";
+            label1.Text = senderButton.Text + " vient d'être cliqué";
+            senderButton.Text = "Bouton cliqué";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
